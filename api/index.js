d97dbb51f805554283dceafa24a937fa52d1dfd8
tripop0876449437 <tripop0876449437@gmail.com>
2025-01-22 00:48:05 +0700

    chreng bg elPostgres

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const app = require('../app'); // นำเข้า Express App
 
 module.exports = (req, res) => {
-  app(req, res); // ส่ง Express App ให้กับ Serverless Environment
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  app(req, res); // ส่ง Express App ให้กับ Serverless Function
 };
