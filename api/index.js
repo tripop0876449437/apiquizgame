6989cd6421789ea32775f274c2e7c9e204987a92
tripop0876449437 <tripop0876449437@gmail.com>
2025-01-22 00:16:04 +0700

    debug

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
+require('pg'); // บังคับให้โหลดไลบรารี pg
 const serverless = require('serverless-http');
-const app = require('../app'); // นำเข้า Express App
+const app = require('../app');
 
 module.exports = serverless(app);
