78d3b8d662aba1a203d04f8412ff484f86e0f975
tripop0876449437 <tripop0876449437@gmail.com>
2025-01-22 11:59:04 +0700

    api quizgmae4

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('../app'); // นำเข้า Express App
-
-module.exports = (req, res) => {
-  console.log(`Incoming request: ${req.method} ${req.url}`);
-  app(req, res); // ส่ง Express App ให้กับ Serverless Function
-};
